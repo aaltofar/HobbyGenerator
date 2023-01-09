e8efc2c1ffab734f4e969734b3cd4b1a89f7db82
Marius Aalto <108743532+aaltofar@users.noreply.github.com>
2023-01-09 12:50:59 +0100

    Fullført
    Ukesoppgave uke 1, hobbygenerator

--- Hobbygenerator/Program.cs
+++ Hobbygenerator/Program.cs
@@ -6,10 +6,18 @@ using System.Threading.Tasks;
 
 namespace Hobbygenerator
 {
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            Random rand = new Random();
+            string[] hobbies = { "Nintendo-spiller", "buseplukker", "strikker", "tåfis-lukter" };
+            string randomHobby = hobbies[rand.Next(hobbies.Length)];
+            Console.WriteLine("Hvem skal få en ny hobby?");
+            string person = Console.ReadLine();
+            Console.WriteLine("Hei " + person + " du har nå blitt en " + randomHobby);
+            Console.ReadLine();
         }
     }
 }
